53c51da3cecf9055260ddb5ae8a3b62858bc6275
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-03-23 19:12:51 +0100

    Started conversion functions

--- src/kuka_eki/conversions.py
+++ src/kuka_eki/conversions.py
@@ -1,1 +1,23 @@
+#  Copyright (c) 2019 Norwegian University of Science and Technology
+#  Use of this source code is governed by the BSD 3-Clause license, see LICENSE
+
+import numpy as np
 from geometry_msgs.msg import Pose, PoseStamped, Transform
+
+
+def orientation_to_abc_in_deg(orientation):
+    quaternion = np.array([orientation.w,
+                           orientation.x,
+                           orientation.y,
+                           orientation.z])
+    abc = np.rad2deg(
+        tf.transformations.euler_from_quaternion(quaternion, 'rzyx'))
+    return abc
+
+
+def pose_to_xyzabc_in_mm_deg(pose):
+    x, y, z = np.array([pose.position.x,
+                        pose.position.y,
+                        pose.position.z]) * 1000.0
+    a, b, c = orientation_to_abc_in_deg(pose.orientation)
+    return [x, y, z, a, b, c]
---
+++ src/kuka_eki/krl.py
@@ -0,0 +1,28 @@
+
+
+class Axis(object):
+    def __init__(self):
+        self.a1 = 0.0
+        self.a2 = 0.0
+        self.a3 = 0.0
+        self.a4 = 0.0
+        self.a5 = 0.0
+        self.a6 = 0.0
+
+    def tolist(self):
+        return [self.a1, self.a2, self.a3, self.a4, self.a5, self.a6]
+
+
+class Pos(object):
+    def __init__(self):
+        self.x = 0.0
+        self.y = 0.0
+        self.z = 0.0
+        self.a = 0.0
+        self.b = 0.0
+        self.c = 0.0
+        self.s = 0
+        self.t = 0
+
+    def tolist(self):
+        return [self.x, self.y, self.z, self.a, self.b, self.c, self.s, self.t]

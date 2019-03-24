bdd348dd471eb9d2e7945192cb9e47736e270a28
Lars Tingelstad <lars.tingelstad@ntnu.no>
2019-03-24 14:34:37 +0100

    Renaming

--- kuka_eki_interface/setup.py
+++ kuka_eki_interface/setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 setup_args = generate_distutils_setup(
-    packages=['kuka_eki'],
+    packages=['kuka_eki_interface'],
     package_dir={'': 'src'}
 )
 
